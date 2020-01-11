abe66cb1ef636b365ca376dd801c355bcf94802c
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-01-10 20:47:03 -0700

    Add test it has attributes

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -9,4 +9,11 @@ class EncryptorTest < Minitest::Test
     assert_instance_of Encryptor, encryptor 
   end
 
+  def test_it_has_attributes
+    encryptor = Encryptor.new('message', '01392', '100120')
+    assert_equal 'message', encryptor.message 
+    assert_equal '01392', encryptor.key 
+    assert_equal '100120', encryptor.date 
+  end
+
 end
