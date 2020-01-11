b9b8cee8acb721988e0d2262a7af6aa8df174540
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-01-10 18:43:52 -0700

    Add Offset class and test_it_exists / test_it_has_attributes

---
+++ lib/offset.rb
@@ -0,0 +1,11 @@
+require 'date'
+
+class Offset
+
+  attr_reader :date
+
+  def initialize(date = Time.now.strftime('%d%m%y')) 
+    @date = date
+  end
+
+end
---
+++ test/offset_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require_relative '../lib/offset'
+
+class OffsetTest < Minitest::Test 
+
+  def test_it_exists 
+    offset = Offset.new('02715')
+    assert_instance_of Offset, offset
+  end 
+
+  def test_it_has_attributes
+    offset1 = Offset.new('040895')
+    assert_equal '040895', offset1.date
+
+    offset2 = mock('offset')
+    offset2.stubs(:date).returns('100120')
+    assert_equal '100120', offset2.date
+  end
+
+end
