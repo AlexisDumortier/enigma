527688677e16482a4bf3612a1ac27be96e06629f
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-01-12 14:35:37 -0700

    Add the encrypt runner file

---
+++ encrypt.rb
@@ -0,0 +1,21 @@
+require_relative "./lib/enigma"
+
+ARGV == ["message.txt", "encrypted.txt"]
+ARGV[0] == "message.txt"
+ARGV[1] == "encrypted.txt"
+
+file = File.open(ARGV[0], "r")
+message = file.read
+
+enigma = Enigma.new
+output = enigma.encrypt(message)
+
+
+file = File.open(ARGV[0], "r")
+message = file.read
+
+encrypted_file = File.open(ARGV[1], "w")
+encrypted_file.write(output[:encryption])
+encrypted_file.close
+
+puts "Created #{ARGV[1]} with the key #{output[:key]} and date #{output[:date]}"
