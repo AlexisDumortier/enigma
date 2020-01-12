db4ebedc362596b993c7a1b2d80caaea5aa0c64b
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-01-11 18:25:36 -0700

    Add tests it can unshift alphabet positions and decrypt

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -4,11 +4,30 @@ require_relative './offset'
 
 class Decryptor < Cryptor
 
-  def initialize(message, key = '', date = '') 
+  attr_reader :decrypt_key, :decrypt_offset, :decrypt_shift
+
+  def initialize(message, key, date = '') 
     super(message, key, date) 
     @decrypt_key = Key.new(key)
     @decrypt_offset = (date == '' ? Offset.new : Offset.new(date))
-    @decrypt_shift = [@decrypt_key.make_keys, @decrypt_offset.make_offsets].transpose.map{|a| a.sum} 
+    @shift = [@decrypt_key.make_keys, @decrypt_offset.make_offsets].transpose.map{|a| a.sum} 
   end
-  
+
+  def unshifted_alphabet_positions
+    result = []
+    message_in_alphabet_positions.each_with_index do |position, index|
+      position.nil? ? result << position : result << (position - message_shifts[index]) % 27 
+    end
+    result
+  end
+
+  def decrypt
+    result = []
+    alphabet = ("a".."z").to_a << " "
+    unshifted_alphabet_positions.each_with_index do |position, index|
+      position.nil? ? result << @message[index] : result << alphabet[position]
+    end
+    { decryption: result.join, key: @decrypt_key.digits, date: @decrypt_offset.date }
+  end
+
 end
--- test/decryptor_test.rb
+++ test/decryptor_test.rb
@@ -8,6 +8,21 @@ class DecryptorTest < Minitest::Test
     decryptor = Decryptor.new('message', '01392', '100120')
     assert_instance_of  Decryptor, decryptor
   end 
-  
 
+  def test_it_can_return_an_array_of_alphabet_positions_for_a_message
+    decryptor = Decryptor.new('keder ohulw!!', '01392', '100120')
+    assert_equal [10, 4, 3, 4, 17, 26, 14, 7, 20, 11, 22, nil, nil], decryptor.message_in_alphabet_positions
+  end
+  
+  def test_it_can_return_an_array_of_unshifted_alphabet_positions
+    decryptor = Decryptor.new('keder ohulw!!', '01392', '100120')
+    assert_equal [5, 14, 18, 20, 12, 9, 2, 23, 15, 21, 10, nil, nil], decryptor.unshifted_alphabet_positions
+  end
+
+  def test_it_can_decrypt_a_given_message
+    decryptor = Decryptor.new("keder ohulw!!", '02715', '040895')
+    expected = { decryption: 'hello world!!' , key: '02715', date: '040895'}
+    assert_equal expected, decryptor.decrypt
+  end
+  
 end
