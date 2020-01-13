a1a36a0de72d9ae6f7a74aa1bdc1780c291c8c11
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-01-12 17:00:06 -0700

    Add the test it can produce a key string

--- lib/crack.rb
+++ lib/crack.rb
@@ -5,6 +5,12 @@ class Crack < Cryptor
   def initialize(message, date = '') 
     @message = message 
     @date = date
-  end 
+  end
   
+  def make_key_string(number)
+    key_string = number.to_s
+    (5 - number.to_s.length).times {key_string = '0' + key_string} 
+    key_string
+  end
+
 end
--- test/crack_test.rb
+++ test/crack_test.rb
@@ -9,4 +9,9 @@ class CrackTest < Minitest::Test
     assert_instance_of Crack, crack 
   end
 
+  def test_it_can_make_a_key_string_from_a_number
+    crack = Crack.new('vjqtbeaweqihssi', '291018')
+    assert_equal '00231', crack.make_key_string(231)
+  end
+
 end
